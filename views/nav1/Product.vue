3a4bff6be13188abd34f0617f6491f4897fcbe75
fanghuaying <945309446@qq.com>
2018-03-12 11:27:25 +0800

    updata

--- views/nav1/Product.vue
+++ views/nav1/Product.vue
@@ -2,50 +2,62 @@
 	<section class="product">
 		<p>产品介绍内容：</p>
 		<div class="product-text">
-			<div v-for="(form,index) in text" :key="index">
+			<div @click="dialogVisibleOne = true">
 				<textarea  class="add-descript" v-model="text"></textarea>
 			</div>
 			<i class="el-icon-circle-close" @click="hiddenI"></i>
 		</div>
-		<div class="product-add">
+		<!-- <div class="product-add">
 			<div class="el-upload el-upload--picture-card" @click="addText()">
 				<i class="el-icon-plus"></i>
 			</div>
-		</div>
+		</div> -->
 		<el-row :gutter="20">
             <el-col :span="6">
-                <div class="product-part" @click="dialogVisible = true">
+                <div class="product-part" @click="dialogVisibleTwo = true">
                     <p class="product-name"><span>名称：</span>视觉设计</p>
 					<p class="product-name"><span>细称：</span>UI级用户体验设计</p>
                     <p class="product-img"><span>图标：</span><img src="../../src/assets/product_01.png"></p>
                 </div>
             </el-col>
             <el-col :span="6">
-                <div class="product-part"  @click="dialogVisible = true">
+                <div class="product-part"  @click="dialogVisibleTwo = true">
                     <p class="product-name"><span>名称：</span>资深网页/UI设计师</p>
 					<p class="product-name"><span>细称：</span>UI级用户体验设计</p>
                     <p class="product-img"><span>图标：</span><img src="../../src/assets/product_02.png"></p>
                 </div>
             </el-col>
             <el-col :span="6">
-                <div class="product-part"  @click="dialogVisible = true">
+                <div class="product-part"  @click="dialogVisibleTwo = true">
                     <p class="product-name"><span>名称：</span>资深JAVA开发工程师</p>
 					<p class="product-name"><span>细称：</span>UI级用户体验设计</p>
                     <p class="product-img"><span>图标：</span><img src="../../src/assets/product_03.png"></p>
                 </div>
             </el-col>
             <el-col :span="6">
-                <div class="product-part"  @click="dialogVisible = true">
+                <div class="product-part"  @click="dialogVisibleTwo = true">
                     <p class="product-name"><span>名称：</span>前端开发工程师</p>
 					<p class="product-name"><span>细称：</span>UI级用户体验设计</p>
                     <p class="product-img"><span>图标：</span><img src="../../src/assets/product_04.png"></p>
                 </div>
             </el-col>
 		</el-row>
-			<!-- 模态框 -->
+		<!-- 模态框一 -->
+		<el-dialog
+		title="提示"
+		:visible.sync="dialogVisibleOne"
+		width="30%"
+		:before-close="oneHandleClose">
+		<textarea cols="30" rows="10" v-model="text"></textarea>
+		<span slot="footer" class="dialog-footer">
+			<el-button @click="dialogVisibleOne = false">取 消</el-button>
+			<el-button type="primary" @click="dialogVisibleOne = false">确 定</el-button>
+		</span>
+		</el-dialog>
+			<!-- 模态框二 -->
 		<el-dialog
 			title="编辑"
-			:visible.sync="dialogVisible"
+			:visible.sync="dialogVisibleTwo"
 			width="40%"
 			:before-close="handleClose"
 			class="dtitle">
@@ -69,8 +81,8 @@
 					</el-upload>
 				</p>
 				<span slot="footer" class="dialog-footer">
-					<el-button @click="dialogVisible = false">取 消</el-button>
-					<el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+					<el-button @click="dialogVisibleTwo = false">取 消</el-button>
+					<el-button type="primary" @click="dialogVisibleTwo = false">确 定</el-button>
 				</span>
 		</el-dialog>
 	</section>
@@ -80,9 +92,10 @@
 	export default {
 	data() {
       return {
-		 	text: ['目前，公司在基于教育培训行业的移动端APP开发，智能工作平台、数据分析系统建设等方面均取得了长足的进展，并不断探索文化电商、内容电商、人文教育数字化等前沿领域。公司将连接线上线下的学习交流空间，搭建高效的互联网学习平台及企业信息化管理体系视为己任，未来亦将突破现有领域，努力推动先进的互联网交互技术和信息管理技术的实践运用。'],
+		 	text: '目前，公司在基于教育培训行业的移动端APP开发，智能工作平台、数据分析系统建设等方面均取得了长足的进展，并不断探索文化电商、内容电商、人文教育数字化等前沿领域。公司将连接线上线下的学习交流空间，搭建高效的互联网学习平台及企业信息化管理体系视为己任，未来亦将突破现有领域，努力推动先进的互联网交互技术和信息管理技术的实践运用。',
 			fileList:[{name: 'qinhan.jpeg', url: 'http://www.shbaoyuantech.com/img/icon-01.png'}],
-			dialogVisible: false,
+			dialogVisibleTwo: false,
+			dialogVisibleOne: false,
 			dialogImageUrl: '',
 			inputName:'视觉设计',
 			inputDetil:'UI级用户设计',
@@ -91,6 +104,13 @@
     methods: {
       handleClose(done) {
         this.$confirm('确认关闭？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {});
+		},
+	  oneHandleClose(done) {
+        this.$confirm('确认更改？')
           .then(_ => {
             done();
           })
@@ -149,6 +169,9 @@
 .product .product-text{
 	position: relative;
 }
+.product .product-text textarea{
+	cursor: pointer;
+}
 .product .product-text i{
 	position: absolute;
 	font-size: 20px;
@@ -190,7 +213,14 @@
 .product .product-duty-details{
     padding-left: 20px;
 }
-/* 模态框 */
+/* 模态框一 */
+.el-dialog__body textarea{
+	width: 100%;
+	border-radius: 6px;
+	color: #455761;
+	font-size:15px;		
+} 
+/* 模态框二 */
 .dtitle .el-input{
 	width: 30%;
 }
@@ -203,4 +233,5 @@
 .product .el-upload--picture-card{
     border-radius: 50%;
 }
+
 </style>
